493264576c2f53d4ef3a13ff2b296a06dfb2597c
pereirj2 <joao.pereira2@mail.dcu.ie>
2022-11-22 13:59:54 +0000

    Read in file function created and fixes

--- src/Assignment2.java
+++ src/Assignment2.java
@@ -1,4 +1,10 @@
 
+import java.io.BufferedInputStream;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.math.BigInteger;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
@@ -12,7 +18,7 @@ import javax.crypto.*;
 class Assignment2
 {
     private static final BigInteger BigInteger = null;
-    public static void main(String [] args)
+    public static void main(String [] args) throws IOException
     {
         BigInteger one = BigInteger.ONE;
         // 1: Generate two distinct 512-bit probable primes p and q
@@ -23,17 +29,17 @@ class Assignment2
         // System.out.println(q);
 
         // 2: Calculate the product of these two primes n = pq
-        BigInteger primeProduct = p.multiply(q);
+        BigInteger n = p.multiply(q);
 
         // 3: Calculate the Euler totient function phi(n) φ(pq) = (p - 1)(q - 1)
-        // BigInteger phi = phi(p, q);
-        BigInteger phi = BigInteger.valueOf(17);
+        BigInteger phi = phi(p, q);
+        // BigInteger phi = BigInteger.valueOf(17);
 
         // 4: You will be using an encryption exponent e = 65537, so you will need to ensure that this is 
         //    relatively prime to phi(n). If it is not, go back to step 1 and generate new values for p and q
 
-        //BigInteger exponent = BigInteger.valueOf(65537);
-        BigInteger exponent = BigInteger.valueOf(40);
+        BigInteger exponent = BigInteger.valueOf(65537);
+        // BigInteger exponent = BigInteger.valueOf(40);
 
         boolean relativePrime = true;
 
@@ -42,14 +48,41 @@ class Assignment2
                 relativePrime = false;
             }
         }
-
+        
         BigInteger d = multiplicateInverse(exponent, phi);
         System.out.println(d);
+        
+        // byte[] fileContent = Files.readAllBytes(Paths.get(args[0]));
+        byte[] fileContent = readFile(args[0]);
+        byte[] fileDigest = sha256Digest(fileContent);
 
+        BigInteger message = new BigInteger(1, fileDigest);
+        BigInteger decrypt = decrypt(message, p, q, d);
 
+        BufferedWriter writer = new BufferedWriter(new FileWriter("Modulus.txt"));
+        writer.write(n.toString(16));
+        writer.close();
 
+        BufferedWriter writer2 = new BufferedWriter(new FileWriter("Signature.txt"));
+        writer2.write(decrypt.toString(16));
+        writer2.close();
+        
     }
 
+    private static byte[] readFile(String filename){
+        File file = new File(filename);
+        byte[] fileContent = (new byte[(int) file.length()]);
+        try {
+            
+            FileInputStream modulus = new FileInputStream(file);
+            BufferedInputStream reader = new BufferedInputStream(modulus);
+            reader.close();
+        }  
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        return fileContent;
+    }
     private static BigInteger phi(BigInteger p, BigInteger q){
         BigInteger one = BigInteger.ONE;
         BigInteger phi = p.subtract(one).multiply(q.subtract(one));
@@ -67,21 +100,6 @@ class Assignment2
         if (y.equals(one))
             return x;
         return gcd(y, x.mod(y));
-    }
-        // function to perform modular exponentiation 
-    private static BigInteger modularExp(BigInteger a, BigInteger x, BigInteger n){
-        BigInteger y = new BigInteger("1");
-        int k = x.bitLength();
-        
-        for(int i = k-1; i >= 0; i--)
-        {
-            y = y.multiply(y).mod(n);
-            if(x.testBit(i))
-            {
-                y = y.multiply(a).mod(n);
-            }
-        }
-        return y;
     }
 
   public static BigInteger multiplicateInverse(BigInteger e, BigInteger phiValue){
@@ -112,7 +130,7 @@ class Assignment2
         }
         return result;
     }
-    private BigInteger decrypt(BigInteger hm, BigInteger p, BigInteger q, BigInteger d){
+    private static BigInteger decrypt(BigInteger hm, BigInteger p, BigInteger q, BigInteger d){
         // h(m)^d (mod n)
         // h(m) = s^e mod(n)
         BigInteger one = BigInteger.ONE;
@@ -130,21 +148,23 @@ class Assignment2
         return m;
       }
 
-    private String sha256Digest(String file) throws NoSuchAlgorithmException{
-        
-        byte[] messageBytes = Files.readAllBytes(Paths.get(file));
-        MessageDigest digest = MessageDigest.getInstance("SHA-256");
-        byte[] messageHash = digest.digest(messageBytes);
-
-        return byteArrayToHex(messageHash);
+    private static byte[] sha256Digest(byte[] file){
+        MessageDigest digest;
+        byte[] messageHash = new byte[0];
+        try {
+            digest = MessageDigest.getInstance("SHA-256");
+            messageHash = digest.digest(file);
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return messageHash;
     }
 
-    private String byteArrayToHex(byte[] byteArray){
+    private static String byteArrayToHex(byte[] byteArray){
         StringBuilder string = new StringBuilder(byteArray.length * 2);
         for(byte b: byteArray)
            string.append(String.format("%02x", b));
         return string.toString();
-    }
     }
 
     //   private static String bytesToHex(byte[] hash) {
@@ -178,6 +198,11 @@ CRT & Multiplicate Inverse - https://www.geeksforgeeks.org/weak-rsa-decryption-c
                              https://loop.dcu.ie/mod/resource/view.php?id=1880897
 Modulus Power - https://www.google.com/search?q=biginteger+mod+to+the+power+of&oq=biginteger+mod+to+the+power+of+&aqs=chrome..69i57.7439j0j1&sourceid=chrome&ie=UTF-8
 Hashing - https://www.baeldung.com/java-digital-signature#:~:text=Technically%20speaking%2C%20a%20digital%20signature,algorithm%20are%20all%20then%20sent.
+          https://www.baeldung.com/java-digital-signature#:~:text=Technically%20speaking%2C%20a%20digital%20signature,algorithm%20are%20all%20then%20sent.
           https://www.cesarsotovalero.net/blog/encoding-encryption-hashing-and-obfuscation-in-java.html
-
+BigInteger to hex - https://stackoverflow.com/questions/11918123/how-to-convert-biginteger-value-to-hex-in-java
+String to BigInteger - https://stackoverflow.com/questions/15717240/how-do-i-convert-a-string-to-a-biginteger
+Read File - https://stackoverflow.com/questions/22630999/cannot-create-path-object-from-a-string
+            https://www.baeldung.com/java-byte-arrays-hex-strings
+File to bytes - https://stackoverflow.com/questions/858980/file-to-byte-in-java
 */
